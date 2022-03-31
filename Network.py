11f30bc3667d02c820919268525e3bb82cd7d98f
jnd31 <49377053+jnd31@users.noreply.github.com>
2022-03-31 13:58:18 -0500

    feeding forward

--- Network.py
+++ Network.py
@@ -1,8 +1,13 @@
 import numpy as np
 
+#sigmoid function
+def sigmoid(z):
+    return 1/(1 + np.exp(-z))
+
+def linear(z):
+    return z
+
 class Network():
-    num_layers = 0
-    layers = []
     def __init__(self, sizes):
         self.num_layers = len(sizes)
         self.layers = sizes
@@ -11,7 +16,7 @@ class Network():
         # array([layer 2]), 
         # ...,
         # array([layer n])]
-        self.biases = [np.random.randn(j, 1) for j in sizes[1:]
+        self.biases = [np.random.randn(j, 1) for j in sizes[1:]]
         # list of lists of numpy arrays, each array gives weights for inputs to each node.
         # [[array([node 1 layer 1]),
         #   array([node 2 layer 1])]
@@ -23,3 +28,12 @@ class Network():
         #   ...,
         #   array([node k layer j])]]
         self.weights = [np.random.randn(j, i) for i, j in zip(sizes[:-1], sizes[1:])]
+
+    # activation is a numpy array of inputs to the first layer
+    # returns a list of numpy arrays of the outputs from each layer
+    # the final layer output is a np array found at a[-1].
+    def forward(self, activation, func=sigmoid):
+        a = [activation]
+        for b, w in zip(self.biases, self.weights):
+            a.append(func(np.dot(w, a[-1]) + b))
+        return a
