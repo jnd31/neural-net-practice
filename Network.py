6f6e2f21dd5d45a1ac94171852cbd081ad54a849
jnd31 <49377053+jnd31@users.noreply.github.com>
2022-04-02 11:48:36 -0500

    | || || |_

--- Network.py
+++ Network.py
@@ -16,7 +16,7 @@ def relu(z):
 class Network():
     # sizes is a list containing the number of nodes in each layer, starting with input layer and ending with output layer.
     # a fn with 2 inputs, 4 neurons, and 3 outputs is thus [2, 4, 3]
-    def __init__(self, sizes):
+    def __init__(self, sizes, zeros=False):
         self.num_layers = len(sizes)
         self.layers = sizes
         # biases = list of numpy arrays, one array for each hidden and output layer.
@@ -24,7 +24,11 @@ class Network():
         # array([layer 2]), 
         # ...,
         # array([layer n])]
-        self.biases = [np.random.randn(j, 1) for j in sizes[1:]]
+        if zeros:
+            self.biases = [np.zeros(j, 1) for j in sizes[1:]]
+            self.weights = [np.zeros(j, i) for i, j in zip(sizes[:-1], sizes[1:])]
+        else:
+            self.biases = [np.random.randn(j, 1) for j in sizes[1:]]
         # list of lists of numpy arrays, each array gives weights for inputs to each node.
         # [[array([node 1 layer 1]),
         #   array([node 2 layer 1])]
@@ -35,9 +39,10 @@ class Network():
         #  [array([node 1 layer j]),
         #   ...,
         #   array([node k layer j])]]
-        self.weights = [np.random.randn(j, i) for i, j in zip(sizes[:-1], sizes[1:])]
+            self.weights = [np.random.randn(j, i) for i, j in zip(sizes[:-1], sizes[1:])]
 
     # activation is a numpy array of inputs to the first layer
+    # should be [[node1, node1, node1], [node2, node2, node2]]
     # returns a list of numpy arrays of the outputs from each layer
     # the final layer output is a np array found at a[-1].
     def forward(self, activation, func=relu):
@@ -53,7 +58,10 @@ class Network():
         exp_inputs = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
         return exp_inputs / np.sum(exp_inputs, axis=1, keepdims=True)
 
-
+    # |  ||
+    # || |_
+    def loss(self, outputs):
+        pass
 # FOR FUTURE REFERENCE, THIS IS HOW YOU MAKE AND SAVE DATA @ME
 # import matplotlib.pyplot as plt
 # fig = plt.figure()
